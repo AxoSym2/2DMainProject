38789d827ba4d40c3eb3f7491c57fe8cf2e2c2bf
Axo_Sym2 <tiger04176672@gmail.com>
2026-05-18 12:58:54 +0900

    [업데이트] 로비UI 추가

---
+++ Assets/Scripts/KH_UI/RobbyUI.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class RobbyUI : DaniTechUIBase
+{
+    [SerializeField] private DaniTechUIButton Button_GameStart;
+    [SerializeField] private DaniTechUIButton Button_GameQuit;
+
+    private void OnEnable()
+    {
+        Button_GameStart.BindOnClickButtonEvent(OnClick_GameStart);
+        Button_GameQuit.BindOnClickButtonEvent(OnClick_GameQuit);
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+
+    public void OnClick_GameStart()
+    {
+        //Debug.Log("게임시작");
+        DaniTechUIManager.Instance.CloseContentUI(DaniTechUIType.RobbyUI);
+    }
+
+    public void OnClick_GameQuit()
+    {
+        //Debug.Log("게임종료");
+        DaniTechGameManager.Inst.SaveAndEndGame();
+    }
+}
