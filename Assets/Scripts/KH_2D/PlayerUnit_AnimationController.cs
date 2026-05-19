71877b4aa18df102e04c91f02df449d80e694b02
Axo_Sym2 <tiger04176672@gmail.com>
2026-05-19 14:20:56 +0900

    [업데이트] 캐릭터 움직임 로직 구현

---
+++ Assets/Scripts/KH_2D/PlayerUnit_AnimationController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public enum PlayerUnitState
+{
+    None,
+    Idle,
+    Run,
+    Die,
+    Hit
+}
+
+public class PlayerUnit_AnimationController : MonoBehaviour
+{
+    [SerializeField] private Animator Animator_playerUnit;
+    private PlayerUnitState _currentState;
+
+    public void SetState(PlayerUnitState state)
+    {
+        if (_currentState == state) return;
+        _currentState = state;
+        switch (_currentState) 
+        {
+            case PlayerUnitState.Idle:
+                Animator_playerUnit.SetBool("IsMoving", false);
+                break;
+            case PlayerUnitState.Run:
+                Animator_playerUnit.SetBool("IsMoving", true);
+                break;
+            case PlayerUnitState.Hit:
+                Animator_playerUnit.SetTrigger("Hit");
+                break;
+            case PlayerUnitState.Die:
+                Animator_playerUnit.SetTrigger("Die");
+                break;
+        }
+    }
+
+    public void SetDirection(Vector2 moveInput)
+    {
+        Animator_playerUnit.SetFloat("MoveX", moveInput.x);
+        Animator_playerUnit.SetFloat("MoveY", moveInput.y);
+    }
+}
---
+++ Assets/Scripts/KH_2D/PlayerUnit_Move.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class PlayerUnit_Move : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 5f;
+    private Rigidbody2D _rb;
+    private PlayerUnit_AnimationController _animController;
+    private Vector2 _moveInput;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _animController = GetComponent<PlayerUnit_AnimationController>();
+    }
+
+    private void Update()
+    {
+        _moveInput.x = Input.GetAxisRaw("Horizontal");
+        _moveInput.y = Input.GetAxisRaw("Vertical");
+        _moveInput.Normalize();
+
+        if (_moveInput != Vector2.zero)
+        {
+            _animController.SetState(PlayerUnitState.Run);
+            _animController.SetDirection(_moveInput);
+        }
+        else 
+        {
+            _animController.SetState(PlayerUnitState.Idle);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.linearVelocity = _moveInput * _moveSpeed;
+    }
+}
