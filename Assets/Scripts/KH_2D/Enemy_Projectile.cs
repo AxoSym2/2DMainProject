5bb92eff6fa0ab0a821e19e63d5f132f5c4ce0a0
Axo_Sym2 <tiger04176672@gmail.com>
2026-05-27 12:53:50 +0900

    [수정] 투사체 방향 수정

--- Assets/Scripts/KH_2D/Enemy_Projectile.cs
+++ Assets/Scripts/KH_2D/Enemy_Projectile.cs
@@ -18,6 +18,10 @@ public class Enemy_Projectile : MonoBehaviour
         _speed = speed;
         _direction = direction.normalized;
         _prefabPath = prefabPath;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+
         AutoReturn().Forget();
     }
 
