2f23ea804fa93e439e23166448b242f782070794
Axo_Sym2 <tiger04176672@gmail.com>
2026-06-04 15:34:11 +0900

    [수정] 체력바 flip버그 수정

---
+++ Assets/Scripts/KH_UI/HealthBarFixer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class HealthBarFixer : MonoBehaviour
+{
+    private void LateUpdate()
+    {
+        if(transform.lossyScale.x < 0)
+        {
+            Vector3 scale = transform.localScale;
+            scale.x = -scale.x;
+            transform.localScale = scale;
+        }
+    }
+}
